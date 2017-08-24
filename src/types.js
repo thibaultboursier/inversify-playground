a1827c53e0a0e6cde23ea7fe408c6e1b07411345
thibaultboursier <boursier.thibault@gmail.com>
2017-08-25 01:06:27 +0200

    fix: removing JS files in src dir

--- src/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.TYPES = {
-    ModuleA: Symbol('ModuleA'),
-    ModuleB: Symbol('ModuleB'),
-    ModuleBProvider: Symbol('ModuleBProvider'),
-    ModuleC: Symbol('ModuleC')
-};
-//# sourceMappingURL=types.js.map

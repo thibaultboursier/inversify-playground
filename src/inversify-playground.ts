63c488e7cfb5a08d059d536231c4c4542b921166
thibaultboursier <boursier.thibault@gmail.com>
2017-08-31 01:08:54 +0200

    fix: adding interface for ModuleABuilder

--- src/inversify-playground.ts
+++ src/inversify-playground.ts
@@ -3,11 +3,12 @@ declare function require(module: string);
 import { container } from './ioc.config';
 import { TYPES } from './types';
 import ModuleABuilder from './builders/ModuleABuilder';
+import IModuleABuilder from './typings/module-a-builder.d';
 
 const items: Array<string> = ['foo', 'bar'];
 
 items.forEach((item: string, index: number) => {
-    const moduleABuilder = container.get(TYPES.ModuleABuilder);
+    const moduleABuilder = container.get<IModuleABuilder>(TYPES.ModuleABuilder);
     const moduleA = moduleABuilder
         .setIndex(index)
         .setItem(item)
